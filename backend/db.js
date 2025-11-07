59fc196af4f51e81a04dd5ae774952cb66e25e34
hadyat2018 <hadyatali246@gmail.com>
2025-11-07 14:50:14 +0300

    PostgreSQL Render configuration

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,20 @@
-// backend/db.js
+require('dotenv').config();
 const { Pool } = require('pg');
 
+// Connexion PostgreSQL (Render)
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+pool.connect()
+  .then(() => console.log("✅ Connecté à PostgreSQL (Render)"))
+  .catch(err => console.error("❌ Erreur de connexion :", err));
 
 module.exports = pool;
